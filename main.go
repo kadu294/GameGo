3726fc9fb80f03780d11ca68965a1a32ce955ba6
Carlos Eduardo Kuhnen <cadukuhnen@gmail.com>
2024-08-14 11:31:21 -0300

    fazendo teste pra ver se aparece uma telinha

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import "github.com/hajimehoshi/ebiten/v2"
 
+type Game struct {
+}
+
+// atualiza logica do jogo
+func (g *Game) Update() error {
+	return nil
+}
+
+// desenha objetos do jogo
+func (g *Game) Draw(screen *ebiten.Image) {
+
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return outsideWidth, outsideHeight
+}
 func main() {
-	fmt.Println("Teste")
+	g := &Game{}
+
+	err := ebiten.RunGame(g)
+	if err != nil {
+		panic(err)
+	}
 }
